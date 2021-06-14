1252ddd75b55b647bd985a8058485e80b0c6006b
sswellington <wellws@pm.me>
2021-06-13 21:52:06 -0300

    feat: add unittest on test_equality

--- test/Financial/__init__.py
+++ test/Financial/__init__.py
@@ -1,5 +1,5 @@
-from .test_currency import *
-from .test_equality import *
+from .test_currency import Test_Currency
+from .test_equality import Test_Equality
 from .test_exchange_rate import *
 from .test_multiplication import Test_Multiplication
 from .test_plus import *
--- test/Financial/test_equality.py
+++ test/Financial/test_equality.py
@@ -1,29 +1,32 @@
+import unittest
+
+
 from Financial import Money, Franc, Dollar
 
 
-
-def test_dollar():
-    assert Dollar(4).equals(Dollar(4))    
-    assert Dollar(5).equals(Dollar(5))
-    
-    assert False == Dollar(5).equals(Dollar(4))
-    assert False == Dollar(5).equals(Dollar(6))
-    
-    
-def test_franc():
-    assert (Franc(5).equals(Franc(5)))
-    assert False == Franc(5).equals(Franc(6))
-
-
-def test_money():
-    assert Money(4).equals(Money(4))    
-    assert Money(5).equals(Money(5))
-    
-    assert False == Money(5).equals(Money(4))
-    assert False == Money(5).equals(Money(6))
+class Test_Equality(unittest.TestCase):
+    def test_dollar(self):
+        assert Dollar(4).equals(Dollar(4))    
+        assert Dollar(5).equals(Dollar(5))
+        
+        self.assertFalse(Dollar(5).equals(Dollar(4)))
+        self.assertFalse(Dollar(5).equals(Dollar(6)))
+        
+        
+    def test_franc(self):
+        assert (Franc(5).equals(Franc(5)))
+        self.assertFalse(Franc(5).equals(Franc(6)))
 
 
-def test_inter():
-    assert False == Dollar(5).equals(Franc(5))
-    assert False == Dollar(5).equals(Franc(6))
+    def test_money(self):
+        assert Money(4).equals(Money(4))    
+        assert Money(5).equals(Money(5))
+        
+        self.assertFalse(Money(5).equals(Money(4)))
+        self.assertFalse(Money(5).equals(Money(6)))
+
+
+    def test_inter(self):
+        self.assertFalse(Dollar(5).equals(Franc(5)))
+        self.assertFalse(Dollar(5).equals(Franc(6)))
 
